58b0a9f639b3617ba48b93387f64b60e41c97c9b
willie.smith.chen <williesmithchen@gmail.com>
2015-05-23 15:47:32 +0800

    FileServer

---
+++ NodeJS/HttpFileServer.js
@@ -0,0 +1,9 @@
+var http = require('http');
+var fs = require('fs');
+var server = http.createServer(function (req,res) {
+  	fs.createReadStream(process.argv[3]).pipe(res);
+})
+server.listen(Number(process.argv[2]));
+
+// //Sample Ans Need Add:
+// res.writeHead(200, { 'content-type': 'text/plain' });
